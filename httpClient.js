e7142715e32b54468e0461559e3a20aabda3c4ef
Neuromantex <rupert735@hotmail.com>
2016-05-20 12:34:46 +0200

    http first 2 modules

--- httpClient.js
+++ httpClient.js
@@ -2,10 +2,9 @@ var http = require('http');
 
 var address = process.argv[2];
 
-http.get(address,function(response){
-	response.setEncoding("utf8");
-	response.on("data",function(data){
-		console.log(data);
-	});
-	
+ http.get(process.argv[2], function (response) {
+       response.setEncoding('utf8')
+       response.on('data', console.log)
+       response.on('error', console.error)
+     }).on('error', console.error)
 });
--- httpCollect.js
+++ httpCollect.js
@@ -2,16 +2,19 @@ http = require('http');
 
 var address = process.argv[2];
 
-
 http.get(address, function (response) {
 	var receivedA = new Array();
 	response.setEncoding("utf8");
 	response.on("data", function (data) {
 		receivedA.push(data);
 	});
-	response.on("error", fuction(err) {
-		return console.log(err);
-	}
-		response.on("end", )
-
+	response.on("error", function (err) {
+		return console.error;
 	});
+	response.on("end", function () {
+		var phrase=receivedA.join("");
+		console.log(phrase.length);
+		console.log(phrase);
+	});
+
+}).on('error', console.error);
